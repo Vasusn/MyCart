9dcbb5235bff932282292e75fcda423bd548c415
Vasusn <vasunageshri@gmail.com>
2023-04-16 13:57:47 +0530

    commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,24 +1,18 @@
-
 import 'package:flutter/material.dart';
+import 'consts/consts.dart';
 
-
-Future<void> main() async {
- 
-  runApp(const MaterialApp(
-    debugShowCheckedModeBanner: false,
-    home: MyApp(),
-  ));
+void main() {
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
- 
-    return const MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: Text('hello')
+    return MaterialApp(
+      title: appname,
+      theme: ThemeData(),
     );
   }
 }
