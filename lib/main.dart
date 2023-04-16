83cb6fe5c63503a1cc1b3538eea0fc8ef3a6619d
Vasusn <vasunageshri@gmail.com>
2023-04-16 11:17:40 +0530

    commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,9 @@
-// ignore: unused_import
-import 'package:clgapp/pages/food/Recommanded_food_page.dart';
-// ignore: unused_import
-import 'package:clgapp/pages/food/popular_food_details.dart';
+
 import 'package:flutter/material.dart';
-import 'package:get/get.dart';
 
-import 'pages/home/main_food_page.dart';
 
-void main() {
+Future<void> main() async {
+ 
   runApp(const MaterialApp(
     debugShowCheckedModeBanner: false,
     home: MyApp(),
@@ -19,11 +15,10 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const GetMaterialApp(
+ 
+    return const MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: MainFoodPage(),
-      //home: PopularFoodDetail(),
-      // home: RecommendedFoodDetail(),
+      home: Text('hello')
     );
   }
 }
